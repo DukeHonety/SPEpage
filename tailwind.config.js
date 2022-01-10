f757af090275976c911a9b46d7a94a781344c939
Alperen Yarouv <ayarouv@gmail.com>
2022-01-10 08:45:29 -0800

    marginsetting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      inset: {
+        '3px': '3px',
+      },
+      margin: {
+        '20': '20px',
+      },
+    },
     fontFamily: {
       pontano: ['Pontano Sans', 'sans-serif'],
       lobster2: ['Lobster Two', 'cursive'],
+    },
+    colors:{
+      buttonH:{
+        DEFAULT: 'transparent',
+        50: '#66a7c5',
+        75: '#74D6D2',
+        100: '#07A39D',
+        200: '#009993',
+        300: '#008F89',
+        400: '#00857F',
+      },
+    },
+    container: {
     },
   },
   plugins: [],
